a255ea9e38b7db1bca6d67ec30b795a7e4598398
olivermueller <oli.mueller@gmail.com>
2024-10-29 20:51:59 +0100

    Last minute changes before lecture 01

--- Week_4_lecture/LM_by_hand.py
+++ Week_4_lecture/LM_by_hand.py
@@ -1,3 +1,6 @@
+# create conda environment: conda create -n mods python=3.11
+# run streamlit app: streamlit run LM_by_hand.py
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,7 +10,7 @@ path = "./Advertising.csv"
 df = pd.read_csv(path)
 
 # Sliders for beta0 and beta1
-beta0 = st.slider("Select a value for beta 0", -50.0, 50.0, 12.0)
+beta0 = st.slider("Select a value for beta 0", -20.0, 20.0, 12.0)
 beta1 = st.slider("Select a value for beta 1", -1.0, 1.0, -0.1)
 
 # Calculate the prediction and the residuals
@@ -21,6 +24,8 @@ RSS = (df["Epsilon"]**2).sum()
 fig, ax = plt.subplots()
 ax.scatter(df["TV"], df["Sales"])
 ax.plot(df["TV"], df["Prediction"], color="red")
+# fix limits of the y-axis to -100 and 100
+ax.set_ylim(-10, 40)
 fig.suptitle("Sales ~ beta0 + beta1*TV")
 plt.title(f"RSS: {str(round(RSS, 2))}")
 
